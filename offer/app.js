1a46ea76c31a7b6e8554c6cf0489f43fa5ae48ed
DivyaPremanantha <premananthadivya@gmail.com>
2020-12-30 12:21:54 +0530

    Add initial commit

---
+++ offer/app.js
@@ -0,0 +1,80 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+const databaseManager = require('./databaseManager');
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+exports.offerHandler = async (event) => {
+	console.log(event);
+
+	switch (event.httpMethod) {
+		case 'DELETE':
+			return deleteOffer(event);
+		case 'GET':
+			return getOffer(event);
+		case 'POST':
+			return saveOffer(event);
+		case 'PUT':
+			return updateOffer(event);
+		default:
+			return sendResponse(404, `Unsupported method "${event.httpMethod}"`);
+	}
+};
+
+function saveOffer(event) {
+	const offer = JSON.parse(event.body);
+	offer.offerId = Math.floor(100000 + Math.random() * 900000).toString();
+
+	return databaseManager.saveOffer(offer).then(response => {
+		console.log(response);
+		return sendResponse(200, offer.offerId);
+	});
+}
+
+function getOffer(event) {
+	const offerId = event.pathParameters.offerId;
+
+	return databaseManager.getOffer(offerId).then(response => {
+		console.log(response);
+		return sendResponse(200, JSON.stringify(response));
+	});
+}
+
+function deleteOffer(event) {
+	const offerId = event.pathParameters.offerId;
+
+	return databaseManager.deleteOffer(offerId).then(response => {
+		return sendResponse(200, 'DELETE OFFER');
+	});
+}
+
+function updateOffer(event) {
+	const offerId = event.pathParameters.offerId;
+
+	const body = JSON.parse(event.body);
+	const paramName = body.paramName;
+	const paramValue = body.paramValue;
+
+	return databaseManager.updateOffer(offerId, paramName, paramValue).then(response => {
+		console.log(response);
+		return sendResponse(200, JSON.stringify(response));
+	});
+}
+
+function sendResponse(statusCode, message) {
+	const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	return response
+}
---
+++ offer/databaseManager.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+let dynamo = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_NAME = process.env.TABLE_NAME;
+
+module.exports.initializateDynamoClient = newDynamo => {
+	dynamo = newDynamo;
+};
+
+module.exports.saveOffer = offer => {
+	const params = {
+		TableName: TABLE_NAME,
+		Item: offer
+	};
+
+	return dynamo
+		.put(params)
+		.promise()
+		.then(() => {
+			return offer.offerId;
+		});
+};
+
+module.exports.getOffer = offerId => {
+	const params = {
+		Key: {
+			offerId: offerId
+		},
+		TableName: TABLE_NAME
+	};
+
+	return dynamo
+		.get(params)
+		.promise()
+		.then(result => {
+			return result.Offer;
+		});
+};
+
+module.exports.deleteOffer = offerId => {
+	const params = {
+		Key: {
+			offerId: offerId
+		},
+		TableName: TABLE_NAME
+	};
+
+	return dynamo.delete(params).promise();
+};
+
+module.exports.updateOffer = (offerId, paramsName, paramsValue) => {
+	const params = {
+		TableName: TABLE_NAME,
+		Key: {
+			offerId
+		},
+		ConditionExpression: 'attribute_exists(offerId)',
+		UpdateExpression: 'set ' + paramsName + ' = :v',
+		ExpressionAttributeValues: {
+			':v': paramsValue
+		},
+		ReturnValues: 'ALL_NEW'
+	};
+
+	return dynamo
+		.update(params)
+		.promise()
+		.then(response => {
+			return response.Attributes;
+		});
+};
